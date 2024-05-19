55c41913143a15a0517bd3e64efc4bc37f9c0425
Billy Tahirou Ouattara <ouattarabilly33@hotmail.com>
2024-05-18 22:16:32 -0700

    initial

---
+++ todo.py
@@ -0,0 +1,17 @@
+#TODO: 
+
+""" 
+
+    1. Migrate the dataset to colab
+    2. Setup code to connect to colab at the beginining of a run
+    3. Change the paths to outputs, checkpoints, etc... to match path in google
+       drive
+    4. After saving the most recently trained model, delete the previous model
+    5. Look more into the parameter of the training model
+        a. Number of epochs'
+        b. Number of iterations
+        c. Using captions
+        d. Classifier free guidance
+        e. Batch size, learning rate, adam_weight_decay -> 0.0125 for clip though, use_f16
+
+"""
